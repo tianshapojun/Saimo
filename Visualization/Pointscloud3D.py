f0affe8a523352315ce89677a74cfa918d9ff5c2
tianshapojun <383033930@qq.com>
2023-11-14 11:20:38 +0800

    Create Pointscloud3D.py

---
+++ Visualization/Pointscloud3D.py
@@ -0,0 +1,153 @@
+#read bin file, https://zhuanlan.zhihu.com/p/371254250
+import numpy as np
+import mayavi.mlab as mlab
+import torch
+import json
+
+def check_numpy_to_torch(x):
+    if isinstance(x, np.ndarray):
+        return torch.from_numpy(x).float(), True
+    return x, False
+
+
+def rotate_points_along_z(points, angle):
+    """
+    Args:
+        points: (B, N, 3 + C)
+        angle: (B), angle along z-axis, angle increases x ==> y
+    Returns:
+
+    """
+    points, is_numpy = check_numpy_to_torch(points)
+    angle, _ = check_numpy_to_torch(angle)
+
+    cosa = torch.cos(angle)
+    sina = torch.sin(angle)
+    zeros = angle.new_zeros(points.shape[0])
+    ones = angle.new_ones(points.shape[0])
+    rot_matrix = torch.stack((
+        cosa,  sina, zeros,
+        -sina, cosa, zeros,
+        zeros, zeros, ones
+    ), dim=1).view(-1, 3, 3).float()
+    points_rot = torch.matmul(points[:, :, 0:3], rot_matrix)
+    points_rot = torch.cat((points_rot, points[:, :, 3:]), dim=-1)
+    return points_rot.numpy() if is_numpy else points_rot
+
+
+def boxes_to_corners_3d(boxes3d):
+    """
+        7 -------- 4
+       /|         /|
+      6 -------- 5 .
+      | |        | |
+      . 3 -------- 0
+      |/         |/
+      2 -------- 1
+    Args:
+        boxes3d:  (N, 7) [x, y, z, dx, dy, dz, heading], (x, y, z) is the box center
+
+    Returns:
+    """
+    boxes3d, is_numpy = check_numpy_to_torch(boxes3d)
+
+    template = boxes3d.new_tensor((
+        [1, 1, -1], [1, -1, -1], [-1, -1, -1], [-1, 1, -1],
+        [1, 1, 1], [1, -1, 1], [-1, -1, 1], [-1, 1, 1],
+    )) / 2
+
+    corners3d = boxes3d[:, None, 3:6].repeat(1, 8, 1) * template[None, :, :]
+    corners3d = rotate_points_along_z(corners3d.view(-1, 8, 3), boxes3d[:, 6]).view(-1, 8, 3)
+    corners3d += boxes3d[:, None, 0:3]
+
+    return corners3d.numpy() if is_numpy else corners3d
+
+
+def draw_gt_boxes3d(gt_boxes3d, fig, color=(1,1,1), line_width=1, draw_text=False, text_scale=(1,1,1), color_list=None):
+    ''' Draw 3D bounding boxes
+    Args:
+        gt_boxes3d: numpy array (n,8,3) for XYZs of the box corners
+        fig: mayavi figure handler
+        color: RGB value tuple in range (0,1), box line color
+        line_width: box line width
+        draw_text: boolean, if true, write box indices beside boxes
+        text_scale: three number tuple
+        color_list: a list of RGB tuple, if not None, overwrite color.
+    Returns:
+        fig: updated fig
+    ''' 
+    num = len(gt_boxes3d)
+    for n in range(num):
+        b = gt_boxes3d[n]
+        if color_list is not None:
+            color = color_list[n] 
+        if draw_text: mlab.text3d(b[4,0], b[4,1], b[4,2], '%d'%n, scale=text_scale, color=color, figure=fig)
+        for k in range(0,4):
+            #http://docs.enthought.com/mayavi/mayavi/auto/mlab_helper_functions.html
+            i,j=k,(k+1)%4
+            mlab.plot3d([b[i,0], b[j,0]], [b[i,1], b[j,1]], [b[i,2], b[j,2]], color=color, tube_radius=None, line_width=line_width, figure=fig)
+
+            i,j=k+4,(k+1)%4 + 4
+            mlab.plot3d([b[i,0], b[j,0]], [b[i,1], b[j,1]], [b[i,2], b[j,2]], color=color, tube_radius=None, line_width=line_width, figure=fig)
+
+            i,j=k,k+4
+            mlab.plot3d([b[i,0], b[j,0]], [b[i,1], b[j,1]], [b[i,2], b[j,2]], color=color, tube_radius=None, line_width=line_width, figure=fig)
+    #mlab.show(1)
+    #mlab.view(azimuth=180, elevation=70, focalpoint=[ 12.0909996 , -1.04700089, -2.03249991], distance=62.0, figure=fig)
+    return fig
+
+if __name__== "__main__":
+
+    # lidar_path换成自己的.bin文件路径
+    pointcloud = np.fromfile('D:/Code/Other/Data/000008.bin', dtype=np.float32, count=-1).reshape([-1, 4])
+    
+    x = pointcloud[:, 0]  # x position of point
+    y = pointcloud[:, 1]  # y position of point
+    z = pointcloud[:, 2]  # z position of point
+
+    
+    r = pointcloud[:, 3]  # reflectance value of point
+    d = np.sqrt(x ** 2 + y ** 2)  # Map Distance from sensor
+    
+    degr = np.degrees(np.arctan(z / d))
+    
+    vals = 'height'
+    if vals == "height":
+        col = z
+    else:
+        col = d
+    
+    fig = mlab.figure(bgcolor=(0, 0, 0), size=(640, 500))
+    mlab.points3d(x, y, z,
+                        col,  # Values used for Color
+                        mode="point",
+                        colormap='spectral',  # 'bone', 'copper', 'gnuplot'
+                        # color=(0, 1, 0),   # Used a fixed (r,g,b) instead
+                        figure=fig,
+                        )
+
+
+    with open('D:/Code/Other/Data/000008.json', 'r') as f:
+        bbox = json.loads(f.read())
+    points = boxes_to_corners_3d(np.array(bbox['bboxes_3d']))
+    print(points.shape)
+    
+    
+    x1, y1, z1 = (0, 1, 1)  # | => pt1
+    x2, y2, z2 = (1, 1, 1)  # | => pt2
+    x3, y3, z3 = (0, 0, 1)  # | => pt3
+    x4, y4, z4 = (1, 0, 1)  # | => pt4
+    x5, y5, z5 = (0, 1, 0)  # | => pt5
+    x6, y6, z6 = (1, 1, 0)  # | => pt6
+    x7, y7, z7 = (0, 0, 0)  # | => pt7
+    x8, y8, z8 = (1, 0, 0)  # | => pt8
+
+
+    box_points = np.array([[[x6, y6, z6], [x8, y8, z8], [x7, y7, z7],
+                            [x5, y5, z5], [x2, y2, z2], [x4, y4, z4],
+                            [x3, y3, z3], [x1, y1, z1]]])
+
+    draw_gt_boxes3d(points,fig)
+
+
+    mlab.show()
